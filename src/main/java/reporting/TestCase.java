a6a49b28a63167f0e5e6ecf9ff88583313d0aaff
selvakumar <mselvakumartest@gmail.com>
2025-03-27 16:04:35 -0400

    updated for Report

--- src/main/java/reporting/TestCase.java
+++ src/main/java/reporting/TestCase.java
@@ -1,24 +1,33 @@
 package reporting;
 
+import reporting.ReportThemeFactory.Theme;
+
 public class TestCase {
 
-   public static void main(String[] args) {
+   public void runReport(){
+//	   private Properties properties;
+	   ReportTheme reportTheme = ReportThemeFactory.getReportsTheme(Theme.valueOf("CUSTOM"));
 
-//	        // Create the ReportSettings and ReportTheme objects
-	        ReportSettings reportSettings = new ReportSettings(System.getProperty("user.dir"),"Test");
-//	        reportSettings.setWidth ("500px");
-//	        reportSettings.setHeight("300px");
-
-	        ReportTheme reportTheme = new ReportTheme();
-	        reportTheme.setHeadingBackColor("blue");
-	        reportTheme.setHeadingForeColor("white");
-	        reportTheme.setSubHeadingBackColor("lightgray");
-	        reportTheme.setSubHeadingForeColor("black");
-	        reportTheme.setSubSectionBackColor("white");
-	        reportTheme.setContentBackColor("black");
+//	        // Create the ReportSettings and ReportTheme objects	
+//	        ReportSettings reportSettings = new ReportSettings(System.getProperty("user.dir"),"Test");
+	        ReportSettings	reportSettings = new ReportSettings(System.getProperty("user.dir"),"TestCaseSRM");
+	        reportSettings.setDateFormatString("MMM-dd-yyyy hh:mm:ss a z");
+			reportSettings.setLogLevel(4);
+			reportSettings.setProjectName("SRM");
+			reportSettings.setGenerateExcelReports(false);
+			reportSettings.setGenerateHtmlReports(true);
+			reportSettings.setGenerateSeeTestReports(false);
+			reportSettings.setGeneratePerfectoReports(false);
+			reportSettings
+					.setTakeScreenshotFailedStep(true);
+			reportSettings
+					.setTakeScreenshotPassedStep(true);
+			reportSettings.setConsolidateScreenshotsInWordDoc(false);
+			reportSettings.setConsolidateScreenshotsInPDF(false);
+			reportSettings.setLinkScreenshotsToTestLog(false);
 
 	        // Instantiate the TestReporter class
-	        TestReporter testReporter = new TestReporter(reportSettings,reportTheme);
+			HtmlReport testReporter = new HtmlReport(reportSettings,reportTheme);
 //	        testReporter.reportSettings = reportSettings;
 //	        testReporter.reportTheme = reportTheme;
 
@@ -29,9 +38,9 @@ public class TestCase {
 	        testReporter.startTestCaseReport("TestCase1");
 
 	        // Simulate adding some steps to the report
-	        testReporter.updateTestLog("1", "Step 1", "Test step 1 description", TestReporter.Status.PASS, "screenshot1.png");
-	        testReporter.updateTestLog("2", "Step 2", "Test step 2 description", TestReporter.Status.FAIL, "screenshot2.png");
-	        testReporter.updateTestLog("3", "Step 3", "Test step 3 description", TestReporter.Status.PASS, "screenshot3.png");
+	        testReporter.updateTestLog("1", "Step 1", "Test step 1 description", HtmlReport.Status.PASS, "screenshot1.png");
+	        testReporter.updateTestLog("2", "Step 2", "Test step 2 description", HtmlReport.Status.FAIL, "screenshot2.png");
+	        testReporter.updateTestLog("3", "Step 3", "Test step 3 description", HtmlReport.Status.PASS, "screenshot3.png");
 
 	        // End the test report (close the table and HTML tags)
 	        testReporter.endTestReport();
